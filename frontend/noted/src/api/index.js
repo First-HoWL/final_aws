651eb97e34fa2d3cff2f4d032504dfd4ebf59c6a
student <student@L02-19>
2026-07-01 18:16:09 +0300

    new ip

--- frontend/noted/src/api/index.js
+++ frontend/noted/src/api/index.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000"
+const BASE_URL = import.meta.env.VITE_API_URL || "http://44.197.2.29:8000"
 
 const api = axios.create({
     baseURL: BASE_URL,
